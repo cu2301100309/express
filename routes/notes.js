443d38b40145f9c45f35a719bdb42440216620fb
cu2301100309 <2301100309uk@cyber-u.ac.jp>
2026-01-26 16:38:34 +0900

    add cat api

--- routes/notes.js
+++ routes/notes.js
@@ -5,7 +5,7 @@ var router = express.Router();
 
 // 接続情報を設定
 const { MongoClient } = require("mongodb");
-const uri = "mongodb+srv://cu2301100309:b6yhM5a.h@cu2301100309.wtudtg8.mongodb.net/?appName=cu2301100309";
+const uri = "";
 const client = new MongoClient(uri);
 
 router.get('/', async (req, res) => {
